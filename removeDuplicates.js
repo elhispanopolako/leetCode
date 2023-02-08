85ea5a99c6b0547e7ad1b5ccfc86c060dafd710e
twozniak <twozniak@consdata.com>
2023-02-08 15:49:18 +0100

    remove duplicates

---
+++ removeDuplicates.js
@@ -0,0 +1,36 @@
+//mine
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    let idx = 1
+    let result = []
+    for (let num of nums) {
+        if (num !== nums[idx]) {
+            result.push(num)
+        }
+        idx++
+    }
+    return result
+};
+//other
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    for (i = 0; i < nums.length; i++) {
+        if (nums[i] == nums[i + 1]) {
+            nums.splice(i, 1);
+            i--;
+        }
+    }
+};
+//other2
+var removeDuplicates = function (nums) {
+    let set = new Set(nums);
+    nums.length = 0;
+    nums.push(...set.values())
+    return set.size;
+};
