986bf8c5f2bc317e40d6c15bc21eeea9e169ddc1
twozniak <twozniak@consdata.com>
2023-02-11 14:48:42 +0100

    sqrt and addBinary

---
+++ JavaScript/Sqrt(x).js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+    let res = x
+    for (let i = 1; i <= x + 1; i++) {
+        if (i * i > x) return i - 1;
+    }
+    return res;
+};
