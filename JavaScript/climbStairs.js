ebc036e3691a604cab5455c21901d4a20e24bff1
twozniak <twozniak@consdata.com>
2023-02-11 15:05:01 +0100

    climb stairs with fibonacci

---
+++ JavaScript/climbStairs.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+    let n1 = 0
+    let n2 = 1
+    let nextTerm;
+    for (let i = 0; i < n; i++) {
+        nextTerm = n1 + n2
+        n1 = n2
+        n2 = nextTerm
+    }
+    return nextTerm
+};
