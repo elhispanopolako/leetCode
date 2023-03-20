b3a4bb911393294206d49237778bfbd992bd0081
twozniak <twozniak@consdata.com>
2023-03-20 17:10:43 +0100

    canplaceFlowers

---
+++ JavaScript/canPlaceFlowers.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function (flowerbed, n) {
+    let ones = flowerbed.filter(f => f == 1)
+    let zeros = flowerbed.filter(f => f == 0)
+    return zeros.length - ones.length == n
+};
+//ultimate solution
+var canPlaceFlowers = function (flowerbed, n) {
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (flowerbed[i] === 1) continue;
+
+        if (
+            (i === 0 || flowerbed[i - 1] === 0) &&
+            (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)
+        ) {
+            flowerbed[i] = 1;
+            n--;
+        }
+    }
+
+    return n <= 0;
+};
