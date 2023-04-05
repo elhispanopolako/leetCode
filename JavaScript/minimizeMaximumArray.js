24429af620acd23338c72380ca4f402b3fddda32
twozniak <twozniak@consdata.com>
2023-04-05 08:13:33 +0200

    minimize array

---
+++ JavaScript/minimizeMaximumArray.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimizeArrayValue = function (nums) {
+    let sum = 0
+    let res = 0
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i]
+        res = Math.max(res, Math.ceil(sum / (i + 1)))
+    }
+    return res
+};
