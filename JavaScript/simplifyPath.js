70e433d2809fd374bcbf5142aab8e5a1a8aaf353
twozniak <twozniak@consdata.com>
2023-04-12 17:35:51 +0200

    simplify path

---
+++ JavaScript/simplifyPath.js
@@ -0,0 +1,11 @@
+var simplifyPath = function (path) {
+    const simplifiedPath = [];
+    const dirs = path.split("/");
+
+    for (const dir of dirs) {
+        if (dir === "" || dir === ".") continue;
+        dir === ".." ? simplifiedPath.pop() : simplifiedPath.push(dir);
+    }
+
+    return "/" + simplifiedPath.join("/");
+};
